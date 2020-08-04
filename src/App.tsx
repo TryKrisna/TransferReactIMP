8fb655e9b5e6cf1469e8c1ab3dca1a60537c3024
rjchow <me@rjchow.com>
2020-08-04 21:41:35 +0800

    step 1: add oa-verify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { text } from "@storybook/addon-knobs";
+import { verify } from "@govtechsg/oa-verify";
 
 export const App: React.FunctionComponent = () => {
   const [textFieldContents, setTextFieldContents] = useState("");
+  const [verificationResults, setVerificationResults] = useState("Nothing Here Yet");
+  useEffect(() => {
+    async function verifyDocument() {
+      try {
+        setVerificationResults(
+          JSON.stringify(await verify(JSON.parse(textFieldContents), { network: "ropsten" }), null, 2)
+        );
+        console.log(verificationResults);
+      } catch (e) {
+        setVerificationResults("Invalid JSON");
+      }
+    }
+    verifyDocument();
+  }, [textFieldContents]);
   return (
     <div>
       <div className="flex content-center  text-lg text-black min-h-screen bg-white-700">
@@ -11,7 +25,7 @@ export const App: React.FunctionComponent = () => {
           <textarea className="w-9/12 m-6" onChange={(event) => setTextFieldContents(event.target.value)}></textarea>
         </div>
         <div className="w-1/3 mr-auto bg-gray-500 flex item-center content-center justify-center">
-          <div className="w-9/12 m-6 text-center">{textFieldContents.length}</div>
+          <pre className="w-9/12 m-6 text-left">{verificationResults}</pre>
         </div>
       </div>
     </div>
